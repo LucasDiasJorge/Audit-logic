04be2892cf7d9f46c443a62d3a06ec3e944e5505
Lucas Dias Jorge <lucas_jorg@hotmail.com>
2026-03-21 22:53:20 -0300

    feat(service): implement ClientService

---
+++ Audit-Project/Service/ClientService.cs
@@ -0,0 +1,31 @@
+﻿using Audit_Project.Domain;
+using Audit_Project.Repository;
+using Audit_Project.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Audit_Project.Service;
+
+/// <summary>
+/// Implements business logic for Client entities.
+/// </summary>
+public class ClientService : IClientService
+{
+    private readonly IClientRepository _repository;
+
+    public ClientService(IClientRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<Client> GetClientByIdAsync(Guid id) => _repository.GetByIdAsync(id);
+
+    public Task<IEnumerable<Client>> GetAllClientsAsync() => _repository.GetAllAsync();
+
+    public Task AddClientAsync(Client client) => _repository.AddAsync(client);
+
+    public Task UpdateClientAsync(Client client) => _repository.UpdateAsync(client);
+
+    public Task DeleteClientAsync(Guid id) => _repository.DeleteAsync(id);
+}
