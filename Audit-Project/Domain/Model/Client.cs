7c34571380b5ac76e4ec26fdccebb4cb59522376
Lucas Dias Jorge <lucas_jorg@hotmail.com>
2026-03-21 22:51:48 -0300

    feat(domain): add Client model

---
+++ Audit-Project/Domain/Model/Client.cs
@@ -0,0 +1,29 @@
+﻿namespace Audit_Project.Domain;
+
+/// <summary>
+/// Represents a client entity with a name and company.
+/// </summary>
+[AuditableEntity("clients")]          // log → client_log (automático)
+public class Client : BaseModel
+{
+    /// <summary>
+    /// The name of the client.
+    /// </summary>
+    public string ClientName { get; set; }
+
+    /// <summary>
+    /// The name of the company associated with the client.
+    /// </summary>
+    public string CompanyName { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the Client class.
+    /// </summary>
+    /// <param name="clientName">The client's name.</param>
+    /// <param name="companyName">The company name.</param>
+    public Client(string clientName, string companyName)
+    {
+        ClientName = clientName;
+        CompanyName = companyName;
+    }
+}
